f1da9a30d70a8630c1b548ff74d8b323542b1e4a
Saurabh Srivastava <ssrivastava@attune.gshapps.net>
2017-05-12 18:21:06 +0530

    appconifg update

--- src/com/omnie/shareyourviewservice/config/AppConfig.java
+++ src/com/omnie/shareyourviewservice/config/AppConfig.java
@@ -12,32 +12,23 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 
-@ImportResource({ "WEB-INF/config/applicationContext.xml" } )
-public class AppConfig  {
-	 
-	
-	
+@ImportResource({ "WEB-INF/config/applicationContext.xml" })
+public class AppConfig {
+
 	@Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasenames("/i18n/usermsg", "/i18n/user-err", "/i18n/validation-msg");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setUseCodeAsDefaultMessage(false);
-        return messageSource;
-    }
-	
-	@Bean
-	public LocaleResolver localeResolver(){
-	SessionLocaleResolver  resolver = new SessionLocaleResolver ();
-	   resolver.setDefaultLocale(new Locale("en"));
-	   return resolver;
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames("/i18n/usermsg", "/i18n/user-err", "/i18n/validation-msg");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setUseCodeAsDefaultMessage(false);
+		return messageSource;
 	}
 
-	
-	
-	
-	
-	
-	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver resolver = new SessionLocaleResolver();
+		resolver.setDefaultLocale(new Locale("en"));
+		return resolver;
+	}
 
 }
