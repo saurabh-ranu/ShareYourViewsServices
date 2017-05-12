906fd88c72b0a5c0989701f5a89135894f67da88
Saurabh Srivastava <ssrivastava@attune.gshapps.net>
2017-05-12 15:48:47 +0530

    caching implementation using ehcache.

---
+++ src/com/omnie/shareyourviewservice/config/CachingConfig.java
@@ -0,0 +1,21 @@
+package com.omnie.shareyourviewservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {
+	
+	@Bean(name = "ehcache")
+    public EhCacheManagerFactoryBean getEhCacheManagerFactoryBean(){
+          EhCacheManagerFactoryBean  cacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+          Resource configLocation = new  ClassPathResource("ehcache.xml");
+          cacheManagerFactoryBean.setConfigLocation(configLocation);
+          return cacheManagerFactoryBean;
+    }
+}
